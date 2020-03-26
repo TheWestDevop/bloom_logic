eb5b1a52846f2ec66e534107f52aa124ea613408
thewestdevop <oyeniyiadedayo@gmail.com>
2020-03-26 17:49:46 +0100

    cancel subscription added

--- routes/api.php
+++ routes/api.php
@@ -25,7 +25,7 @@ Route::post('reset-password','UserController@reset_password');
 //user logout
 Route::post('logout','UserController@logout');
 
-//upload id card, photo, inspection reports & any docuemnt
+//upload id card, photo, inspection reports & any docuemnt 
 Route::post('upload','UserController@upload');
 
 //update profile and settings
@@ -79,50 +79,47 @@ Route::get('packages','TransactionController@subscriptions');
 //verify transaction
 Route::any('verify-transaction','TransactionController@verify');
 
-//cancel subscription
-Route::any('cancel-subscription','TransactionController@cancel_subscription');
-
 //test apis
 Route::any('find-requests','TripController@find_requests');
 
 Route::any('get-otp','UserController@getOtp');
 
-// Route::any('test-otp',function(){
-
-
-//     $password=rand(100000,999999);
-
-//     return implode('-',str_split($password,3));
-
-//     $endpoint = 'https://rest.nexmo.com/sms/json';
-//     $otpArray = array(
-//         "api_key"=>"f1c0068b",
-//         "api_secret"=>"I9c9R0WZfZkGBXaU",
-//         "to"=>"+2348084049966",
-//         "from"=>"NEXMO",
-//         "text"=>"Use 087-654 to verify your number"
-//     );
-//     $params = http_build_query($otpArray);
-//     $ch = curl_init();
-//     curl_setopt($ch, CURLOPT_URL,$endpoint);
-//     curl_setopt($ch, CURLOPT_RETURNTRANSFER,true);
-//     curl_setopt($ch, CURLOPT_POST, true);
-//     curl_setopt($ch, CURLOPT_POST, 1);
-//     curl_setopt($ch, CURLOPT_POSTFIELDS, $params);
-// //    curl_setopt($ch, CURLOPT_HTTPHEADER, array('Authorization: Bearer '.$jwt,'Content-Type: application/json','Accept: application/json'));
-//     $response = curl_exec($ch);
-//     curl_close($ch);
-
-//     $response=json_decode($response);
-
-//     if($response->messages[0]->status==0){
-//         return TRUE;
-//     }else{
-//         return FALSE;
-//     }
-
-// //    return uniqid();
-// //
-// //    $password=implode('-',str_split(rand(100000,999999),2));
-// //    return $password;
-// });
+Route::any('test-otp',function(){
+    
+    
+    $password=rand(100000,999999);
+    
+    return implode('-',str_split($password,3));
+    
+    $endpoint = 'https://rest.nexmo.com/sms/json';
+    $otpArray = array(
+        "api_key"=>"f1c0068b",
+        "api_secret"=>"I9c9R0WZfZkGBXaU",
+        "to"=>"+2348084049966",
+        "from"=>"NEXMO",
+        "text"=>"Use 087-654 to verify your number"
+    );
+    $params = http_build_query($otpArray);
+    $ch = curl_init(); 
+    curl_setopt($ch, CURLOPT_URL,$endpoint); 
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER,true); 
+    curl_setopt($ch, CURLOPT_POST, true);
+    curl_setopt($ch, CURLOPT_POST, 1); 
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $params); 
+//    curl_setopt($ch, CURLOPT_HTTPHEADER, array('Authorization: Bearer '.$jwt,'Content-Type: application/json','Accept: application/json'));
+    $response = curl_exec($ch); 
+    curl_close($ch); 
+    
+    $response=json_decode($response);
+    
+    if($response->messages[0]->status==0){
+        return TRUE;
+    }else{
+        return FALSE;
+    }
+    
+//    return uniqid();
+//    
+//    $password=implode('-',str_split(rand(100000,999999),2));
+//    return $password;
+});
